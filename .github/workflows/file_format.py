6f149d0749c07616b42a62655690874d71663f96
KhronosWebservices <webservices@khronosgroup.org>
2026-07-13 13:28:26 -0400

    license: Add headers

--- .github/workflows/file_format.py
+++ .github/workflows/file_format.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# SPDX-FileCopyrightText: The Khronos Group Inc.
+
 """
 Basic file formatting checker.
 Validates that all files use UTF-8 encoding, have no BOM, and have LF line endings.
-SPDX license identifier: CC0-1.0
+SPDX-License-Identifier: CC0-1.0
 """
 
 import sys
